974a5908fe378cdbdbe914c9a4c29ca0115276a4
Febryan1453 <brayenfebryan@gmail.com>
2024-06-19 20:38:49 +0700

    Add penjelasan

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => 'febryan/check-even-odd',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'name' => 'febryan2/check-even-odd2',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'fb23a96b2f3f9118c1b86b935e9f4d2f967b9e56',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        'febryan/check-even-odd' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+        'febryan2/check-even-odd2' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'fb23a96b2f3f9118c1b86b935e9f4d2f967b9e56',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
